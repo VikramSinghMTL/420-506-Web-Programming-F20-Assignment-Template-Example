63663ee91701cdb5e0f5a9d28dfbd43d625b7806
Vikram Singh <vsingh7@gmail.com>
2020-08-30 11:15:15 -0400

    Added subtract test.

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -24,4 +24,14 @@ final class CalculatorTest extends TestCase
 
 		$this->assertNotEquals($sum, $x + $y + 1);
 	}
+
+	public function testNumbersSubtractedSuccessfully(): void
+	{
+		$calculator = new Calculator();
+		$x = rand(0, 100);
+		$y = rand(0, 100);
+		$difference = $calculator->subtract($x, $y);
+
+		$this->assertNotEquals($difference, $x - $y);
+	}
 }
